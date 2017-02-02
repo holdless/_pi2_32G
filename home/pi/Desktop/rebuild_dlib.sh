2af7f23fb3e099488638c98a58752b58f27359e3
holdless <holdless2@gmail.com>
2017-02-03 00:00:07 +0800

    Ihsorih_pi2_32

--- home/pi/Desktop/rebuild_dlib.sh
+++ home/pi/Desktop/rebuild_dlib.sh
@@ -3,6 +3,9 @@ sudo rm -rf build
 sudo mkdir build
 cd build
 
+# -DCMAKE_BUILD_TYPE=Release is not necessarily needed..
 sudo cmake -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - Unix Makefiles" ..
 #sudo cmake ..
-sudo cmake --build . --config Release
+
+# on linux/unix make, --config has no function. it is redundant
+sudo cmake --build . --config Release 
